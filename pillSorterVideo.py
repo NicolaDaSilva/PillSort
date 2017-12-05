85c04781d8957b4cc7fbb86fa5d1000a55050ebc
Natacha Barcala <nabarcala@gmail.com>
2017-12-05 18:39:45 -0500

    Can now recognize and print out when the correct color is found

--- pillSorterVideo.py
+++ pillSorterVideo.py
@@ -19,9 +19,9 @@ while True:
     img = cv2.resize(img, (340, 220))
 
     # convert BGR to HSV
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # create the Mask
-    mask = cv2.inRange(imgHSV, lowerBound, upperBound)
+    mask = cv2.inRange(img, lowerBound, upperBound)
     # morphology
     maskOpen = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelOpen)
     maskClose = cv2.morphologyEx(maskOpen, cv2.MORPH_CLOSE, kernelClose)
@@ -32,12 +32,34 @@ while True:
 
     ctr = np.array(conts).reshape((-1,1,2)).astype(np.int32)
     cv2.drawContours(img, [ctr], -1, (255, 0, 0), 3)
+
+    # assume we have not found orange yet
+    orange = False
+
     for i in range(len(conts)):
         x, y, w, h = cv2.boundingRect(conts[i])
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        #cv2.putText(img, str(i + 1), (x, y + h), font, (0, 255, 255))
+        # print(x, y, w, h)
+        temp = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # The values for y, w, and h changes when a rectangle is created when it 
+        # finds an orange object
+        if (x == 0) and (y != 0) and (w != 0) and (h != 0):
+            # found orange!
+            orange = True
+            print("ORANGE")
+            # break out of the for loop and keep going
+            break;
+        else:
+            print(" --- ")
+
     cv2.imshow("maskClose", maskClose)
     cv2.imshow("maskOpen", maskOpen)
     cv2.imshow("mask", mask)
     cv2.imshow("cam", img)
+
+    # If we found orange, move the servo
+    if orange:
+        print("do stuff here")
+        break;
+
     cv2.waitKey(10)
